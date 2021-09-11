45ca7fec5de825cc16433ef7a84916b9df05d098
kundan <987kundanrai@gmail.com>
2021-09-11 23:58:19 +0530

    some changes in app.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import Textform from './components/Textform';
+import React, { useState } from 'react'
+
+// import About from './components/About';
 
 function App() {
+  const [mode, setMode] = useState('light')
+  const toggleMode = () => {
+    if (mode === 'dark') {
+      setMode('light')
+      document.body.style.backgroundColor='white'
+      
+    }
+    else {
+      setMode('dark')
+      document.body.style.backgroundColor='grey'
+    }
+  }
   return (
     <>
-      <Navbar title="TextUtils" home="Home"/>
+      <Navbar title="TextUtils" aboutText="About" mode={mode} toggleMode={toggleMode} />
       <div className="container">
-      <Textform heading='Enter text to analyse'/>
+        <Textform heading='Enter text to analyse' />
+        {/* <About></About> */}
       </div>
+
     </>
   );
 }
